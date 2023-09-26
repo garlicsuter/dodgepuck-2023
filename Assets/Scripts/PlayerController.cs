bd613bc3065129edd833f01b2c8ac988db4b8adf
Adam <23calva@live.elida.k12.oh.us>
2023-09-26 10:53:39 -0400

    movement works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float xRange;
+    public float yRange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +17,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        //if(transform.position.x > )
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
         //Debug.Log("moveHorizontal: " + moveHorizontal);
 
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        Debug.Log("movement variable is = " + movement);
+        transform.Translate(movement * speed * Time.deltaTime);
+        
+        //Debug.Log("movement variable is = " + movement);
 
         //if (Input.GetKeyDown(KeyCode.D))
         //{
